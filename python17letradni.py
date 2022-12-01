3c3d5e0af7ff6e91591dbfa722c06ef7f8cf6130
DomingoCastro <domingocastro1998@outlook.com>
2022-12-01 16:25:32 +0100

    commit

--- python17letradni.py
+++ python17letradni.py
@@ -1,11 +1,13 @@
 from math import trunc
 print("Escribe tu DNI")
 dni = (input())
-print (len(dni))
-print(dni.isdigit())
-dni = int(dni)
-operacion = (dni - (trunc(dni / 23) * 23))
-print (str(operacion))
-cadena = "TRWAGMYFPDXBNJZSQVHLCKET" 
-letra = cadena[operacion]
-print(letra)
+if (dni.isdigit() == False):
+    print("DEBE SER SOLO NUMEROS")
+elif len(dni) != 8 :
+    print ("DEBE TENER 8 CARACTERES")
+else:
+    dni = int(dni)
+    operacion = (dni - (trunc(dni / 23) * 23))
+    cadena = "TRWAGMYFPDXBNJZSQVHLCKET" 
+    letra = cadena[operacion]
+    print("Su letra del dni es " + letra)
